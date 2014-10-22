55ec73797ad48404a21c27a4e31f2b78b408f4ba
Rishi Mittal <rimittal@adobe.com>
2014-10-22 15:34:59 -0600

    Java Programs

---
+++ Java/UploadClassifications.java
@@ -0,0 +1,48 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+		String urlname = "https://api.omniture.com/genesis/rest/3.1/index.html?method=Import.UploadClassifications";
+		
+		// *** The username and secret can be obtained through your Adobe Partner Integrations contact ***
+		AdobeMarketingPartnerAPI client = new AdobeMarketingPartnerAPI("USERNAME", "PASSWORD");
+
+		/* Format the upload data in the format as give here - https://marketing.adobe.com/developer/en_US/documentation/genesis/r-uploadclassifications
+		 * The original formating of the data below is:
+		 *
+		 * {
+			"integrationCode":"8a4b8b735cb2f696",
+			"metricName":"Tracking ID",
+			"columnNames":[
+			"Key", "Name", "Color"],
+			"rows":[
+						["1234", "Name 1", "Red"],
+						["1235", "Name 2", "White"]
+					],
+			"endOfBlock":"1"
+			}
+		 * 
+		 * */
+		
+		String postData = "{"
+				+ "\"integrationCode\":\"8a4b8b735cb2f696\","
+				+ "\"metricName\":\"Tracking ID\","
+				+ "\"columnNames\":["
+						+ "\"Key\", \"Name\", \"Color\"],"
+						+ "\"rows\":["
+							+ "[\"1234\", \"Name 1\", \"Red\"],"
+							+ "[\"1235\", \"Name 2\", \"White\"]"
+							+ "],"
+				+ "\"endOfBlock\":\"1\"}";
+		
+		// Use method callPost for using Import.UploadClassifications
+		String jsonResponse = client.callPOST(urlname, postData);
+		
+		// Capture the fileID for future use e.g. for Import.CheckClassificationUpload method
+		System.out.println("Response: "+jsonResponse);
+		
+	}
+	
+}
